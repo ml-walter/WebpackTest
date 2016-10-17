7f9b2395478ec9d6818fb534ec6bb0ed2708f452
Walter <xx159357222@gmail.com>
2016-10-17 15:52:20 +0800

    get rectangle_area

--- src/js/CropImage.js
+++ src/js/CropImage.js
@@ -1,7 +1,100 @@
 const HammerEffect = require('./HammerEffect');
 
-class CropImage{
-    constructor(){
+class CropImage {
+    constructor() {
+        var CROP_IMAGE_SIZE = 900;
+        var hitArea = $('.hit_area').get(0);
+        var canvas = $('#canvas').get(0);
+        var hammer = new HammerEffect(hitArea, data => this.drawCanvas(data));
+        this.source = new Image();
+
+        // event trigger
+        this.source.onload = (e) => {
+            hammer.init(this.source.width, this.source.height, canvas.width, canvas.height)
+        };
+
+        $(".upload_input").on("change", (e) => {
+            var input = event.target.files[0];
+            // canvasResize
+            canvasResize(input, {
+                width: CROP_IMAGE_SIZE,
+                height: CROP_IMAGE_SIZE,
+                crop: true,
+                quality: 100,
+                callback: (data, width, height, imageData) => {
+                    console.log("onParseImageComplete", width, height);
+                    if (width < CROP_IMAGE_SIZE && height < CROP_IMAGE_SIZE) {
+                        alert("選取的圖片太小張，換一張試試看");
+                    } else {
+                        this.source.src = data;
+                    }
+                }
+            });
+        });
+
+        $('#getRectangleResult').on('click', e => {
+            this.getRectangleResult();
+        });
+    }
+
+    drawCanvas(hammerData) {
+        /**
+         * @param {{scale:number,angle:number,x:number,y:number}} hammerData 
+         * */
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        if (hammerData) {
+            ctx.save();
+
+            var radian = hammerData.angle * Math.PI / 180;
+            var scale = hammerData.scale;
+
+            ctx.translate(hammerData.x, hammerData.y);
+            ctx.rotate(radian);
+            ctx.scale(scale, scale);
+
+            ctx.drawImage(this.source, hammerData.centerX * -1, hammerData.centerY * -1);
+            ctx.restore();
+        }
+    }
+    getRectangleResult() {
+        //Get rectangle_area's coordinate and width and height in webpage
+        var _rectangle = $('.rectangle_area'),
+            _rectangle_offset = _rectangle.offset(),
+            rectangle_area = {
+                x: _rectangle_offset.left,
+                y: _rectangle_offset.top,
+                width: _rectangle.width(),
+                height: _rectangle.height()
+            };
+        console.log(rectangle_area);
+        //Get canvas's coordinate and width and height in webpage
+        var _canvas = $('#canvas'),
+            _canvas_ctx = $('#canvas').get(0),
+            _canvas_offset = _canvas.offset(),
+            canvas_area = {
+                x: _canvas_offset.left,
+                y: _canvas_offset.top,
+                width: _canvas.width(),
+                height: _canvas.height()
+            }
+        console.log(canvas_area);
+        //determine rectangle_area and canvas
+        var rectangle_result = {
+            x: Math.round(rectangle_area.x - canvas_area.x),
+            y: Math.round(rectangle_area.y - canvas_area.y),
+            width: Math.round(rectangle_area.width),
+            height: Math.round(rectangle_area.height)
+        }
+        console.log(rectangle_result);
+
+        //create elemnt canvas to store the rectangle_area
+        var rectangle_canvas = document.createElement('canvas');
+        rectangle_canvas.width = rectangle_result.width;
+        rectangle_canvas.height = rectangle_result.height;
+        var rectangle_ctx = rectangle_canvas.getContext('2d');
+        rectangle_ctx.drawImage(_canvas_ctx, rectangle_result.x, rectangle_result.y, rectangle_result.width, rectangle_result.height, 0, 0, rectangle_result.width, rectangle_result.height);
+        $('#RectangleImg').attr('src', rectangle_canvas.toDataURL("image/jpeg"));
 
     }
 }
--- src/js/entry.js
+++ src/js/entry.js
@@ -1,56 +1,9 @@
 import 'index.scss';
 import 'index.jade';
-
-const HammerEffect = require('./HammerEffect');
+require('../lib/all');
+const CropImage = require('./CropImage');
 $(() => {
-    var CROP_IMAGE_SIZE = 1024;
-
-    $(".upload_input").on("change",function(event){                
-        var input = event.target.files[0];
-        // canvasResize
-        canvasResize(input, {
-            width: CROP_IMAGE_SIZE,
-            height: CROP_IMAGE_SIZE,
-            crop: true,            
-            quality: 100,               
-            callback: function(data , width , height, imageData){
-                console.log("onParseImageComplete",width,height);
-                if (width < CROP_IMAGE_SIZE && height < CROP_IMAGE_SIZE) {
-                    alert("選取的圖片太小張，換一張試試看");
-                } else {
-                    source.src = data;            
-                }                        
-            }
-        });
-    });
-
-
-    var hitArea = $( '.hit_area' ).get( 0 );
-    var canvas = $( '#canvas' ).get( 0 );
-    var hammer = new HammerEffect( hitArea, data => drawCanvas( data ) );
-
-    var source = new Image();
-    source.src = require('../img/img_1024.png');
-    source.onload = () => hammer.init( source.width, source.height , canvas.width , canvas.height );    
-    
-    /**
-     * @param {{scale:number,angle:number,x:number,y:number}} hammerData 
-     * */
-    function drawCanvas( hammerData ) {
-        var ctx = canvas.getContext('2d');
-        ctx.clearRect( 0, 0, canvas.width, canvas.height );        
-        if ( hammerData ) {
-            ctx.save();
-
-            var radian = hammerData.angle * Math.PI / 180;
-            var scale = hammerData.scale;
-
-            ctx.translate( hammerData.x, hammerData.y );
-            ctx.rotate( radian );
-            ctx.scale( scale, scale );
-
-            ctx.drawImage( source, hammerData.centerX*-1, hammerData.centerY*-1 );
-            ctx.restore();
-        }
-    }
+    var _CropImage = new CropImage();
+    //Img Default
+    _CropImage.source.src=require('../img/img_1024.png');
 });
